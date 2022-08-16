b20e509933cd27c87d0ff1980bb50a7c386279ff
Navelwriter <noahlee0514@gmail.com>
2022-08-16 11:48:13 -0400

    Early work on FFT

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 #include <Wire.h>
 #include <adc.h>
 #include <Bounce2.h>
-
+#include "arduinoFFT.h"
 #define Button1 17
 #define ADC2_GPIO 34
+#define R_IN 26 //Right audio input
+#define L_IN 27 //Left audio input
 int ledState = LOW;
 int val = 0;
 int mem = 0;
@@ -47,7 +49,16 @@ void button_ISR();
     U8x8 Text Only Example: No RAM usage, direct communication with display controller. No graphics, 8x8 Text only.
 
 */
+arduinoFFT FFT = arduinoFFT(); // create an instance of the FFT class
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+
+const uint16_t samples = 128; // size of the FFT buffer
+double vReal_R[samples]; // real part of the FFT output       
+double vImag_R[samples]; // imaginary part of the FFT output 
+double vReal_L[samples];
+double vImag_L[samples];
+int16_t wave_R[samples]; // waveform data for the FFT input      
+int16_t wave_L[samples];
 
 int map(int s, int a1, int a2, int b1, int b2) {
   return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
@@ -58,6 +69,7 @@ void setup() {
   adc_init();
   adc_gpio_init(ADC2_GPIO);
   adc_select_input(2); // select ADC input 2
+  analogReadResolution(12); // set the ADC resolution to 12 bit
   
   pinMode(LED_BUILTIN, OUTPUT); // set Built_in_LED as output
   digitalWrite(LED_BUILTIN, !ledState); // set Built_in_LED to LOW
@@ -66,9 +78,14 @@ void setup() {
   b.attach(Button1); // attach Button1 to the Bounce object
   b.interval(10); // set the bounce interval to 10ms
   Serial.begin(9600); 
-  //print hello message on the serial monitor
-  Serial.println("Hello World!");
   
+  u8g2.setFont(u8g2_font_6x10_mf);
+  u8g2.setDrawColor(1);
+  u8g2.setFontPosTop();       
+  u8g2.clearBuffer();
+  u8g2.drawStr( 0, 0, "Start AudSpec");
+  u8g2.sendBuffer();
+  delay(1000);
 }
 
 // create a u8g2 rectangle class with functions to draw and fill it
@@ -107,6 +124,30 @@ void loop() {
   if (val != mem) {
     mem = val;
   }
+
+  for (int i = 0; i < samples; i++) { 
+    wave_R[i] = analogRead(R_IN); // read the waveform data from the ADC
+    wave_L[i] = analogRead(L_IN);
+    delayMicroseconds(21); // delay for 21 microseconds to get the next sample
+  }       
+
+  for (int i = 0; i < samples; i++) { // perform the FFT on the waveform data
+    vReal_R[i] = (wave_R[i] - 2048) * 3.3 / 4096.0; // convert the waveform data to a voltage
+    vReal_L[i] = (wave_L[i] - 2048) * 3.3 / 4096.0;
+    vImag_R[i] = 0; // set the imaginary part to 0
+    vImag_L[i] = 0;
+  }
+
+  FFT.Windowing(vReal_R, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD); // apply the windowing function
+  FFT.Windowing(vReal_L, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD); 
+ 
+  FFT.Compute(vReal_R, vImag_R, samples, FFT_FORWARD); // compute the FFT
+  FFT.Compute(vReal_L, vImag_L, samples, FFT_FORWARD);
+ 
+  FFT.ComplexToMagnitude(vReal_R, vImag_R, samples); // compute the magnitude of the FFT output
+  FFT.ComplexToMagnitude(vReal_L, vImag_L, samples);
+
+  FFT.
   uint8_t char_width = u8g2.getStrWidth("Hello World");
   sprintf(event_str, "ADC: %d, %d", result, val);
   u8g2.setFont(u8g2_font_6x10_mf);
